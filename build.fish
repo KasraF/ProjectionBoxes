f355903c202c3cff1e4ddf92959751bd09c50092
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2020-09-09 13:32:14 -0700

    updated build script

--- build.fish
+++ build.fish
@@ -1,12 +1,27 @@
 #!/usr/bin/fish
+set _pwd (pwd)
+set _log "$_pwd/build.log";
+
 cd vscode
-and yarn run gulp editor-distro
+and echo "Rebuiling VSCode:"
+and echo "  Building the editor-distro..."
+and yarn run gulp editor-distro >> "$_log"
 and cd out-monaco-editor-core
-and yarn link
+and echo "  Linking monaco-editor-core..."
+and yarn link >> "$_log"
+and echo "  VSCode build succeeded!"
 and cd ../../monaco-editor
-and yarn link monaco-editor-core
-and yarn release
+and echo "Rebuilding Monaco Editor:"
+and echo "  Cleaning the release directory..."
+and rm -rf ./release
+and echo "  Linking monaco-editor-core..."
+and yarn link monaco-editor-core >> "$_log"
+and echo "  Building monaco-editor..."
+and yarn release >> "$_log"
 and cd release
-and yarn link
-and cd ../../MonacoServer/src/main/javascript/
-and yarn build
+and echo "  Linking monaco-editor..."
+and yarn link >> "$_log"
+and echo "  Monaco Editor build succeeded!"
+and cd ../../MonacoServer/
+and echo "Rebuilding Monaco Server:"
+and mvn clean install -T4 >> "$_log";
