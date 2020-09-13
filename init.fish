396f285ef5869bbe1d9e682a454ea0a42821e7a0
Kasra Ferdowsifard <kferdowsifard@gmail.com>
2020-09-13 16:33:56 -0700

    fixed issues with init script

--- init.fish
+++ init.fish
@@ -4,18 +4,24 @@ set root (pwd);
 
 # Build Pyodide
 cd $root/pyodide
-and echo "\n\n====================================================================================="
+and echo
+and echo
+and echo "====================================================================================="
 and echo "==                                    Pyodide                                      =="
 and echo "====================================================================================="
+and echo
 and make
 and cd $root/MonacoServer/src/main/resources/static/pyodide
 and cp -r $root/pyodide/build/* .
 and git checkout webworker.js # Checkout the webworker again, because we modified it
 
 # Build vscode
-and echo "\n\n====================================================================================="
+and echo
+and echo
+and echo "====================================================================================="
 and echo "==                               Visual Stdio Code                                 =="
 and echo "====================================================================================="
+and echo
 and cd $root/vscode
 and yarn
 and yarn compile
@@ -24,9 +30,12 @@ and cd out-monaco-editor-core
 and yarn link
 
 # Build Monaco Editor
-and echo "\n\n====================================================================================="
+and echo
+and echo
+and echo "====================================================================================="
 and echo "==                                 Monaco Editor                                   =="
 and echo "====================================================================================="
+and echo
 and cd $root/monaco-editor
 and yarn link monaco-editor-core
 and yarn
@@ -35,12 +44,16 @@ and cd release
 and yarn link
 
 # Build MonacoServer
-and echo "\n\n====================================================================================="
+and echo
+and echo
+and echo "====================================================================================="
 and echo "==                                 Monaco Server                                   =="
 and echo "====================================================================================="
+and echo
 and cd $root/MonacoServer
 and yarn
 and yarn build
+and mvn clean install
 and cd $root
 and set root $_root
 and exit;
